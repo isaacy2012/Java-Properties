cb35a7f183f979f4c48fd6206b4aec1e68e9922e
isaacy2012 <isaacy2012@gmail.com>
2021-10-03 23:36:21 +1300

    Updated Demo

--- src/demo/CarWithProp.java
+++ src/demo/CarWithProp.java
@@ -7,17 +7,17 @@ import isaacy2012.property.Property;
  * The type Car with prop.
  */
 public class CarWithProp {
-    private final MutableProperty<Integer> _horsepower;
+    private final MutableProperty<Integer> _horsepower = MutableProperty.ofEmpty();
     /**
      * The Horsepower.
      */
-    public final Property<Integer> horsepower;
+    public final Property<Integer> horsepower = Property.ofProperty(_horsepower);
 
-    private final MutableProperty<String> _name;
+    private final MutableProperty<String> _name = MutableProperty.ofEmpty();
     /**
      * The Name.
      */
-    public final MutableProperty<String> name;
+    public final MutableProperty<String> name = MutableProperty.ofProperty(_name);
 
     /**
      * Instantiates a new Car.
@@ -26,10 +26,8 @@ public class CarWithProp {
      * @param horsepower the horsepower
      */
     public CarWithProp(String name, int horsepower) {
-        this._name = MutableProperty.of(name);
-        this.name = MutableProperty.ofProperty(_name);
-        this._horsepower = MutableProperty.of(horsepower);
-        this.horsepower = Property.ofProperty(_horsepower);
+        this._name.set(name);
+        this._horsepower.set(horsepower);
     }
 
     /**
