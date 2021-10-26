8cc1724b5223e3c9c1de9282042a9903f24c9336
isaacy2012 <isaacy2012@gmail.com>
2021-10-27 00:23:02 +1300

    Added MutableLazyProperty

--- Java-Properties/src/main/java/isaacy2012/lazy/Lazy.java
+++ Java-Properties/src/main/java/isaacy2012/lazy/Lazy.java
@@ -1,17 +1,41 @@
 package isaacy2012.lazy;
 
 import isaacy2012.lazy.impl.ImmutableLazyProperty;
+import isaacy2012.lazy.impl.MutableLazyProperty;
 import isaacy2012.property.MutableProperty;
 import isaacy2012.property.Property;
-import isaacy2012.property.ValueProperty;
+import isaacy2012.property.impl.ImmutableValueProperty;
 import isaacy2012.property.impl.MutableValueProperty;
 
+import java.util.function.Supplier;
+
+/**
+ * The interface Lazy.
+ *
+ * @param <T> the type parameter
+ */
 public interface Lazy<T> extends Property<T> {
-    public static <T> Property<T> of(ValueProperty<T> prop) {
-        return new ImmutableLazyProperty<T>(prop);
+    /**
+     * Of property.
+     *
+     * @param <T>      the type parameter
+     * @param prop     the prop
+     * @param supplier the supplier
+     * @return the property
+     */
+    static <T> Property<T> of(ImmutableValueProperty<T> prop, Supplier<T> supplier) {
+        return new ImmutableLazyProperty<>(prop, supplier);
     }
 
-    public static <T> MutableProperty<T> of(MutableValueProperty<T> prop) {
-        return new ImmutableLazyProperty<T>(prop);
+    /**
+     * Of mutable property.
+     *
+     * @param <T>      the type parameter
+     * @param prop     the prop
+     * @param supplier the supplier
+     * @return the mutable property
+     */
+    static <T> MutableProperty<T> of(MutableValueProperty<T> prop, Supplier<T> supplier) {
+        return new MutableLazyProperty<>(prop, supplier);
     }
 }
