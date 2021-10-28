64ff1a237b068b0f28417091df962c53b93c4c0f
isaacy2012 <isaacy2012@gmail.com>
2021-10-29 00:55:20 +1300

    Delete LazyInitialNotNullException.java

--- Java-Properties/src/main/java/isaacy2012/lazy/exception/LazyInitialNotNullException.java
+++
@@ -1,7 +0,0 @@
-package isaacy2012.lazy.exception;
-
-public class LazyInitialNotNullException extends RuntimeException {
-    public LazyInitialNotNullException(String message) {
-        super(message);
-    }
-}
