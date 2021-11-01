12fabe642567c22c09b3ab2c308e899fb92789bd
isaacy2012 <isaacy2012@gmail.com>
2021-11-01 17:36:14 +1300

    Update Lazy.java

--- Java-Properties/src/main/java/isaacy2012/lazy/Lazy.java
+++ Java-Properties/src/main/java/isaacy2012/lazy/Lazy.java
@@ -1,13 +1,7 @@
 package isaacy2012.lazy;
 
-import isaacy2012.lazy.exception.LazyInitialNotNullException;
 import isaacy2012.lazy.impl.ImmutableLazyProperty;
-import isaacy2012.lazy.impl.MutableLazyProperty;
-import isaacy2012.property.MutableProperty;
 import isaacy2012.property.Property;
-import isaacy2012.property.exception.ValueNotInitializedException;
-import isaacy2012.property.impl.ImmutableValueProperty;
-import isaacy2012.property.impl.MutableValueProperty;
 
 import java.util.function.Supplier;
 
